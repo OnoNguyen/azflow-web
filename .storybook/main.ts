0e45a0901bc9458342f2496ffd01bfc86a0ecf86
OnoNguyen <nguyenono@gmail.com>
2024-05-29 18:29:56 +1000

    feat(storybook): created vite storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,33 @@
-import type {StorybookConfig} from "@storybook/react-webpack5";
+export default {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        "@storybook/preset-create-react-app",
-        "@storybook/addon-onboarding",
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@chromatic-com/storybook",
-        "@storybook/addon-interactions",
-    ],
-    framework: {
-        name: "@storybook/react-vite",
-        options: {},
-    },
-    staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-mdx-gfm",
+    "@chromatic-com/storybook",
+  ],
+
+  async viteFinal(config: any) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import("vite");
+
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-dark-mode"],
+      },
+    });
+  },
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
-export default config;
--- src/component/AppRoutes.tsx
+++ src/component/AppRoutes.tsx
@@ -1,17 +1,16 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
-import {Home} from "../pages/home";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/home";
 import App from "../App";
-import {Login} from "../pages/login";
-import {SignUp} from "../pages/signup";
-import {FreeMind} from "../FreeMind";
+import { Login } from "../pages/login";
+import { SignUp } from "../pages/signup";
+import { FreeMind } from "../pages/FreeMind";
 
 export const AppRoutes = () => (
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/app" element={<App/>}/>
-        <Route path="/freemind" element={<FreeMind/>}/>
-        <Route path="/login" element={<Login/>}/>
-    </Routes>
-)
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/app" element={<App />} />
+    <Route path="/freemind" element={<FreeMind />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+);
