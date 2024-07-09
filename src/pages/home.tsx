006a74546bfac496df450d600e1e8f1d8a0027e2
OnoNguyen <nguyenono@gmail.com>
2024-07-09 18:32:42 +1000

    fix(home): updated gql to get audios

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,9 +6,9 @@ import { ErrorNotification } from "@/component/Error";
 import { useAuth } from "@/auth/useAuth.tsx";
 import AudioPlayer from "@/component/AudioPlayer";
 
-const GET_TRACKS = gql`
-  mutation getTrackUrls($userId: String!) {
-    trackUrls(input: { userId: $userId })
+const GET_AUDIOS = gql`
+  mutation getAudiokUrls($userId: String!) {
+    audioUrls(input: { userId: $userId })
   }
 `;
 
@@ -16,16 +16,18 @@ export const Home = () => {
   const [items, setItems] = useState<DraggableListItem[]>([]);
   const { currentUser } = useAuth();
 
-  const [trackUrls, { loading, error, data }] = useMutation(GET_TRACKS);
+  const [audiokUrls, { loading, error, data }] = useMutation(GET_AUDIOS);
 
   useEffect(() => {
     const userId = currentUser?.localAccountId ?? "";
-    trackUrls({ variables: { userId: userId } }).catch((e) => console.error(e));
-  }, [currentUser, trackUrls]);
+    audiokUrls({ variables: { userId: userId } }).catch((e) =>
+      console.error(e),
+    );
+  }, [currentUser, audiokUrls]);
 
   useEffect(() => {
     if (data) {
-      const draggableItems = data.trackUrls.map((url: string) => ({
+      const draggableItems = data.audioUrls.map((url: string) => ({
         key: url,
         content: <AudioPlayer src={url} />,
       }));
